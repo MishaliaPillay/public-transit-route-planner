310eab1848d305794e3f69b87734c2868081b7e2
MishaliaPillay <2435450@students.wits.ac.za>
2024-04-21 19:04:41 +0200

    nav bar responsive

--- src/Pages/Confirmation/Confirmation.jsx
+++ src/Pages/Confirmation/Confirmation.jsx
@@ -1,10 +1,13 @@
 import React from "react"; // Import useState for state management
 import Confetti from "react-confetti";
-
+import { Cheers } from "@phosphor-icons/react";
+import "./confirmation.css";
 const Confirmation = () => {
   return (
     <div>
-      <h4>Payment succesful!!</h4>
+      <h4 className="successTxt">
+        Payment succesful!! <Cheers size={32} />
+      </h4>
       <Confetti
         count={1000}
         colors={["#52006A", "#CD113B", "#FF7600", "#FFA900"]}
